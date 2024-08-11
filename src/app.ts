025a10cccac516767cf39b7faaf061a7c40f01a3
arju10 <arjuarju71@gmail.com>
2024-08-11 21:06:45 +0600

    [Done] Project Setup and Install Dependencies

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working successfully');
+  // console.log(x)
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import config from './config/index';
+import app from './app';
+
+import { Server } from 'http';
+
+process.on('uncaughtException', error => {
+  process.exit(1);
+});
+
+let server: Server;
+async function boostrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`Database is connected successfully`);
+
+    // Server is running
+    app.listen(config.port, () => {
+      console.log(`Server is running on port http://localhost:${config.port}`);
+    });
+  } catch (error) {
+    console.log('Failed to connect Database', error);
+  }
+
+  process.on('unhandledRejection', error => {
+    // console.log("Unhandled Rejection is detected, we are closing our server.............")
+    if (server) {
+      server.close(() => {
+        console.log(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+boostrap();
+
+// console.log(x)
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
