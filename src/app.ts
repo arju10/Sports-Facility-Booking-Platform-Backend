f62a5b08feb94ff2725e8c9faecf76a1ec68fc28
arju10 <arjuarju71@gmail.com>
2026-02-03 17:48:09 +0600

    Fix http://localhost:8081' has been blocked by CORS policy

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,25 @@ import notFound from "./middlewares/notFound";
 const app: Application = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "http://localhost:5173"],
+//     credentials: true,
+//   }),
+// );
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:8081',  
+    'http://localhost:8080',
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
